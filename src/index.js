d05e2ca46515651e2d1182718d6175cd0c597a21
lanoharahap <harahaplano@yahoo.co.id>
2021-08-12 22:00:20 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import "../node_modules/jquery/dist/jquery.min.js";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+
 
 ReactDOM.render(
   <React.StrictMode>
