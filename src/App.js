b9e9207dd9c467978c6bf2633fb65abd4430b9fe
Arman <arman.41@hotmail.com>
2021-09-18 13:46:28 +0700

    add loading done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Home from './components/Home';
+import Loading from './components/Loading';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [top, setTop] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setTop(false), 200)
+    setTimeout(() => setLoading(false), 3000)
+  }, [])
   return (
-    <Router>
-    <Nav />
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route component={Home} />
-    </Switch>
-    <Footer />
-    </Router>
+    <>
+      {loading ? <Loading /> : null}
+      {top ? null : (
+        <Router>
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route component={Home} />
+        </Switch>
+        <Footer />
+        </Router>
+      )}
+    </>
   );
 }
 
